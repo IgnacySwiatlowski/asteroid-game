2693fbfdd2672e986e7c10715d4584b88ab84f79
IgnacySwiatlowski <ignacyswiatlowski@gmail.com>
2025-07-14 20:55:58 +0200

    add collision mechanics

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 import pygame
+from circleshape import CircleShape
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
+from asteroid import Asteroid
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for obj in asteroids:
+            if player.collision(obj):
+                print("Game over!")
+                return
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        player.update(dt)
+
     
 
 if __name__ == "__main__":
